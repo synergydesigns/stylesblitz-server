ca8e265f2d9a985c402453917a10336224ba8134
enahomurphy <enahomurphy@gmail.com>
2018-11-09 12:33:41 +0100

    Add test for get providers by service and location

--- shared/models/provider_test.go
+++ shared/models/provider_test.go
@@ -5,29 +5,60 @@ import (
 
 	config "gitlab.com/synergy-designs/style-blitz/shared/config"
 	"gitlab.com/synergy-designs/style-blitz/shared/models"
+	"gitlab.com/synergy-designs/style-blitz/shared/seeder"
 )
 
-func TestGetUserByID(t *testing.T) {
-	
-	// seed providers 
-	// seed services
-	
+var providerService = models.ProviderDbService{models.Connect(config.LoadConfig())}
+
+func TestGetProvidersByLocationAndService(t *testing.T) {
+
+	seed := seeder.Seeder{}
+
+	seed.Init().LoadData("provider").Seed("provider")
+	seed.LoadData("address").Seed("address")
+	seed.LoadData("category").Seed("category")
+	seed.LoadData("service").Seed("service")
+
+	defer seed.Close()
+
+	testCases := []struct {
+		Title             string
+		ServiceName       string
+		Lat, Long, Radius float64
+		Count             int
+	}{
+		{
+			Title: `should  get provider that 
+				offers services within 1 km readius`,
+			ServiceName: "Extensions",
+			Lat:         6.57747305,
+			Long:        3.36740283,
+			Radius:      1,
+			Count:       2,
+		},
+		{
+			Title: `should  get provider that 
+				offers services within 1 km radius of ajao estate`,
+			ServiceName: "Extensions",
+			Lat:         6.54565167,
+			Long:        3.33274265,
+			Radius:      1,
+			Count:       1,
+		},
+	}
 
 	for _, testCase := range testCases {
-		user, err := db.GetUserByID(testCase.ID)
+		providers, _ := providerService.GetProvidersByServiceAndLocation(
+			testCase.ServiceName,
+			testCase.Lat,
+			testCase.Long,
+			testCase.Radius,
+		)
 
-		if err != nil {
-			if testCase.Error != err.Error() {
-				t.Error("expected", testCase.Error)
-				t.Error("actual", err.Error())
-			}
-		} else {
-			if user != testCase.Response {
-				t.Errorf("expected %d to equal %d", user.ID, testCase.Response.ID)
-				t.Errorf("expected %s to equal %s", user.Name, testCase.Response.Name)
-				t.Errorf("expected %s to equal %s", user.Username, testCase.Response.Username)
-				t.Errorf("expected %s to equal %s", user.Password, testCase.Response.Password)
-			}
+		if len(providers) != testCase.Count {
+			t.Errorf("expected result count %d to equal %d", len(providers), testCase.Count)
 		}
 	}
+
+	seed.Clean()
 }
