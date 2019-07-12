2620f93797a66c09f90dd9c2e470997ff3df57b1
Enaho Murphy <enahomurphy@gmail.com>
2019-07-12 10:52:04 +0100

    movemigrations to go-migrate

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/synergydesigns/stylesblitz-server
+
+require (
+	github.com/99designs/gqlgen v0.8.1
+	github.com/GuiaBolso/darwin v0.0.0-20170210191649-86919dfcf808
+	github.com/aws/aws-lambda-go v1.2.0
+	github.com/fnproject/fdk-go v0.0.0-20190102214815-bd24a5aa63cf
+	github.com/golang-migrate/migrate/v4 v4.5.0
+	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/jinzhu/gorm v1.9.1
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+	github.com/lib/pq v1.0.0
+	github.com/vektah/gqlparser v1.1.1
+	gopkg.in/yaml.v2 v2.2.2
+)
--- migrations/main.go
+++ migrations/main.go
@@ -4,52 +4,54 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
-	"github.com/GuiaBolso/darwin"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-
-	"github.com/synergydesigns/stylesblitz-server/migrations/utils"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 	"github.com/synergydesigns/stylesblitz-server/shared/config"
 )
 
-// Migrate migrates db
-func Migrate() {
+const migrationFolder = "file://migrations/mg"
+
+func main() {
 	config := config.LoadConfig()
-	migrations := utils.GenarateDarwinMigrations(config)
 
 	dbURL := fmt.Sprintf(
 		"postgres://%s:%s@%s:%s/%s?sslmode=%s", config.DBUser,
 		config.DBPassword, config.DBHost, config.DBPort, config.DBName, config.SSLMode,
 	)
+	db, err := sql.Open("postgres", dbURL)
 
-	database, err := sql.Open("postgres", dbURL)
-
-	defer database.Close()
+	defer db.Close()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	driver := darwin.NewGenericDriver(database, darwin.PostgresDialect{})
-	d := darwin.New(driver, migrations, nil)
+	driver, _ := postgres.WithInstance(db, &postgres.Config{})
 
-	err = d.Migrate()
+	// start migration
+	if os.Args[1] == "up" {
+		m, err := migrate.NewWithDatabaseInstance(
+			migrationFolder,
+			"postgres", driver)
+		m.Steps(2)
 
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		infos, err := d.Info()
 		if err != nil {
-			fmt.Println(err)
+			log.Fatal(err)
 		}
-
-		for _, info := range infos {
-			fmt.Printf("%.1f %s %s\n", info.Migration.Version, info.Status, info.Migration.Description)
-		}
-		log.Println("Migration completed")
 	}
-}
 
-func main() {
-	Migrate()
+	// tear down migration
+	if os.Args[1] == "down" {
+		m, err := migrate.New(migrationFolder, dbURL)
+
+		err = m.Down()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
